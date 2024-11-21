6fabbe4281968fdd6beafc2b7a85125268193e7e
unknown <dripplechn@gmail.com>
2024-11-21 21:12:11 +0800

    .

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+using MonoFN.Cecil;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    bool canShoot = true;
+
+    [SerializeField] KeyCode shootKey = KeyCode.Mouse0;
+    [SerializeField] float fireRate = 0.3f;
+    [SerializeField] int damage = 5;
+
+    WaitForSeconds shootWait;
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        shootWait = new WaitForSeconds(fireRate);
+
+    }
+
+    private void Update()
+    {
+        if (!base.IsOwner)
+        {
+            return;
+        }
+
+        if (Input.GetKey(shootKey) && canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        print("Player shot");
+        //if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity, LayerMask.NameToLayer("GameHittable")))
+        {
+
+        }
+        StartCoroutine(CanShootUpdater());
+    }
+
+    IEnumerator CanShootUpdater()
+    {
+        canShoot = false;
+        yield return shootWait;
+        canShoot = true;
+    }
+}
