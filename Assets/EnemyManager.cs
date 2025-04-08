e46332f9eaad4ab37f6b3f8c5d2a693632ad3520
unknown <dripplechn@gmail.com>
2025-04-08 15:09:22 +0800

    healthbar

--- Assets/EnemyManager.cs
+++ Assets/EnemyManager.cs
@@ -1,30 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 
 public class EnemyManager : NetworkBehaviour
 {
-    public string color;
-    public readonly SyncVar<int> health = new SyncVar<int>(10);
+    public readonly SyncVar<float> redHealth = new SyncVar<float>(100);
+    public readonly SyncVar<float> greenHealth = new SyncVar<float>(100);
+    public readonly SyncVar<float> blueHealth = new SyncVar<float>(100);
+    public float maxHealth = 100;
+
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+    public Slider magentaSlider;
+    public Slider cyanSlider;
+    public Slider yellowSlider;
+    public Slider whiteSlider;
+
     // Start is called before the first frame update
 
     [ServerRpc(RequireOwnership = false)]
     public void DamageEnemy(int damage, string color)
     {
-        if (color == this.color)
+        if (color == "red")
         {
-            print("Hit with color " + color + " health is " + health.Value.ToString());
-            health.Value -= damage;
-            if (health.Value < 0)
-            {
-                Despawn();
-            }
+            print("Hit with color " + color + " health is " + redHealth.Value.ToString());
+            redHealth.Value -= damage;
         }
-        else
+        if (color == "green")
         {
-            print("color mismatch");
+            print("Hit with color " + color + " health is " + greenHealth.Value.ToString());
+            greenHealth.Value -= damage;
+        }
+        if (color == "blue")
+        {
+            print("Hit with color " + color + " health is " + blueHealth.Value.ToString());
+            blueHealth.Value -= damage;
+        }
+        if (redHealth.Value <= 0 && greenHealth.Value <= 0 && blueHealth.Value <= 0)
+        {
+            Despawn();
         }
     }
 
@@ -32,5 +50,28 @@ public class EnemyManager : NetworkBehaviour
     public void Despawn()
     {
         gameObject.SetActive(false);
+    }
+
+    //show local value on the healthbars
+    public void updateHealthBar()
+    {
+        redSlider.value = redHealth.Value / maxHealth;
+        greenSlider.value = greenHealth.Value / maxHealth;
+        blueSlider.value = blueHealth.Value / maxHealth;
+        magentaSlider.value = Mathf.Max(0f, Mathf.Min(redHealth.Value, blueHealth.Value)) / maxHealth;
+        cyanSlider.value = Mathf.Max(0f, Mathf.Min(greenHealth.Value, blueHealth.Value)) / maxHealth;
+        yellowSlider.value = Mathf.Max(0f, Mathf.Min(redHealth.Value, greenHealth.Value)) / maxHealth;
+        whiteSlider.value = Mathf.Min(redHealth.Value, blueHealth.Value, greenHealth.Value) / maxHealth;
+    }
+
+    //if undamaged for a time, regens health for the lower color healthbars. (If redhealth = 10, blue health = 8, then bluehealth will regenerate, and the healthbar color will be megenta again)
+    public void regenHealth()
+    {
+
+    }
+
+    public void Update() {
+        updateHealthBar();
+        regenHealth();
     }
 }
