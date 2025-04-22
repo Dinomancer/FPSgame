822f4727028a6ef2f9715816775bc3dd0df4983e
unknown <dripplechn@gmail.com>
2025-04-22 22:08:47 +0800

    more waves

--- Assets/WaveManager.cs
+++ Assets/WaveManager.cs
@@ -20,16 +20,27 @@ public class WaveManager : MonoBehaviour
             }
         }else if (state == 1)    //wave 1
         {
-            spawnpoints[0].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
-            spawnpoints[6].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
-            spawnpoints[24].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
-            spawnpoints[18].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
-            spawnpoints[4].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
-            spawnpoints[8].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
-            spawnpoints[16].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
-            spawnpoints[20].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
-            spawnpoints[12].GetComponent<EnemySpawner>().spawnEnemyServer("magentaEnemy");
             state = 2;
+            spawnpoints[2].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
+            spawnpoints[10].GetComponent<EnemySpawner>().spawnEnemyServer("greenEnemy");
+            spawnpoints[14].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
+            StartCoroutine(SpawnWave2(6f));
         }
+    }
+
+    IEnumerator SpawnWave2(float time)
+    {
+        yield return new WaitForSeconds(time);
+        spawnpoints[0].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
+        spawnpoints[6].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
+        spawnpoints[24].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
+        spawnpoints[18].GetComponent<EnemySpawner>().spawnEnemyServer("redEnemy");
+        spawnpoints[4].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
+        spawnpoints[8].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
+        spawnpoints[16].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
+        spawnpoints[20].GetComponent<EnemySpawner>().spawnEnemyServer("blueEnemy");
+        spawnpoints[12].GetComponent<EnemySpawner>().spawnEnemyServer("magentaEnemy");
+
+
     }
 }
